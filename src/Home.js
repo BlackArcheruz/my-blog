9296066a02b6baaecfad7ce70de10f892e668c50
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-01-10 11:25:55 +0500

    lesson 16

--- src/Home.js
+++ src/Home.js
@@ -1,22 +1,31 @@
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import BlogList from './BlogList';
 
 const Home = () => {
-    // let name = 'Usmon';
+
     const [blogs, setBlogs] = useState([
         { title: 'My new website', body: 'lorem ipsum...', author: 'Muhammad Siddiq', id: 1 },
         { title: 'Welcome party!', body: 'lorem ipsum...', author: 'Others', id: 2 },
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Muhammad Siddiq', id: 3 }
     ]);
 
+    const [name,setName] = useState('Muhammad Siddiq')
+
     const handleDelete = (id)=>{
         const newBlogs = blogs.filter(blog=> blog.id !== id);
         setBlogs(newBlogs);
     }
 
+    useEffect(()=>{
+        console.log('use effect ran');
+        console.log(name);
+    },[name]);
+
     return ( 
         <div className="home">
             <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
+            <button onClick={()=>setName('Usmon')}>change name</button>
+            <p>{name}</p>
         </div>
     );
 }
--- src/Navbar.js
+++ src/Navbar.js
@@ -4,6 +4,7 @@ const Navbar = () => {
             <h1>My Blog</h1>
             <div className="links">
                 <a href="/">Home</a>
+                <a href="/about">About</a>
                 <a href="/create">New Blog</a>
             </div>
         </nav>
