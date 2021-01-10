701fd893b91ad7e1ad7aae19f62f7fc0d0046a4b
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-01-10 10:07:44 +0500

    lesson 15

--- src/Home.js
+++ src/Home.js
@@ -1,4 +1,5 @@
 import { useState } from 'react';
+import BlogList from './BlogList';
 
 const Home = () => {
     // let name = 'Usmon';
@@ -8,14 +9,14 @@ const Home = () => {
         { title: 'Web dev top tips', body: 'lorem ipsum...', author: 'Muhammad Siddiq', id: 3 }
     ]);
 
+    const handleDelete = (id)=>{
+        const newBlogs = blogs.filter(blog=> blog.id !== id);
+        setBlogs(newBlogs);
+    }
+
     return ( 
         <div className="home">
-            {blogs.map((blog)=>(
-                <div className="blog-preview" key={blog.id}>
-                    <h2>{blog.title}</h2>
-                    <p>Written by {blog.author}</p>
-                </div>
-            ))}
+            <BlogList blogs={blogs} title="All Blogs!" handleDelete={handleDelete}/>
         </div>
     );
 }
