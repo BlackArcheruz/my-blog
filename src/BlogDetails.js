f1b5748245edadfd47a9716c78175a34f5b740ab
Muhammad Siddiq Ibragimov <67962548+MuhammadSiddiq-pyxel@users.noreply.github.com>
2021-04-21 06:04:46 +0500

    Update BlogDetails.js

--- src/BlogDetails.js
+++ src/BlogDetails.js
@@ -4,10 +4,10 @@ import {useHistory} from 'react-router-dom';
 
 const BlogDetails = () => {
     const { id } = useParams()
-    const { data:blog,error,isPending } = useFetch('http://localhost:8000/blogs/' + id);
+    const { data:blog,error,isPending } = useFetch('https://api.npoint.io/60d7da8dd16ff43ca34e/blogs/' + id);
     const history = useHistory();
     const handleClick = ()=>{
-        fetch(`http://localhost:8000/blogs/${id}`,{
+        fetch(`https://api.npoint.io/60d7da8dd16ff43ca34e/blogs/${id}`,{
             method:'DELETE',
             headers: {
                 'Content-type':'application/json'
